db5a933e756e077cb6a6103605d67a6649814b5e
eom0j <uyj9011@naver.com>
2023-02-15 03:03:04 +0900

    initial

---
+++ src/main/java/com/presub/presub/PreSubController.java
@@ -0,0 +1,13 @@
+package com.presub.presub;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PreSubController {
+
+    @GetMapping("/")
+    public String helloWorld() {
+        return "To-do-Application !";
+    }
+}
---
+++ src/main/java/com/presub/presub/PresubApplication.java
@@ -0,0 +1,13 @@
+package com.presub.presub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PresubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PresubApplication.class, args);
+	}
+
+}
